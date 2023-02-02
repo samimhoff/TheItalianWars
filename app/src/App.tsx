300c7aedbc5d63bff0fdbcd4c13081b74a22d813
samimhoff <67920224+samimhoff@users.noreply.github.com>
2023-02-02 11:39:15 -0500

    Added initial set up with React, TS, and Tailwind

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className='text-3xl font-bold underline text-red-600'>
+        Simple sample baby
+      </h2>
+    </div>
+  );
+}
+
+export default App;
